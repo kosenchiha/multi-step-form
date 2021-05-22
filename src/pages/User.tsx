32bd01784312aa5bf094cf18502da6bda54dfde5
Vicky <kosenchiha@gmail.com>
2021-05-22 14:34:23 +0100

    create form to collect user information

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -2,12 +2,97 @@ import { Container } from "@material-ui/core";
 import { FC } from "react";
 import FormStepper from "../components/FormStepper";
 import { formSteps } from "../steps";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { Form, Formik } from "formik";
+
+interface FormValues {
+  name: string;
+  role: string;
+  email: string;
+  password: string;
+}
 
 const User: FC = () => {
   return (
     <Container component="main" maxWidth="xs">
       <FormStepper activeStep={0} completedSteps={[]} steps={formSteps} />
-      <p>User page</p>
+      <Formik
+        initialValues={{
+          name: "",
+          role: "",
+          email: "",
+          password: "",
+        }}
+        onSubmit={(values: FormValues) => {
+          console.log(values);
+        }}
+      >
+        {({ values, handleChange }) => (
+          <Form>
+            <TextField
+              required
+              name="name"
+              variant="outlined"
+              placeholder="Enter Your Name"
+              label="Name"
+              onChange={handleChange}
+              value={values.name}
+              margin="normal"
+              fullWidth
+              type="text"
+            />
+
+            <TextField
+              variant="outlined"
+              name="role"
+              placeholder="Enter Your Role"
+              label="Role"
+              onChange={handleChange}
+              value={values.role}
+              margin="normal"
+              fullWidth
+            />
+
+            <TextField
+              required
+              variant="outlined"
+              placeholder="Enter Your Email"
+              name="email"
+              label="Email"
+              onChange={handleChange}
+              value={values.email}
+              margin="normal"
+              fullWidth
+              type="email"
+            />
+
+            <TextField
+              required
+              variant="outlined"
+              placeholder="Enter Your Password"
+              name="password"
+              label="Password"
+              onChange={handleChange}
+              value={values.password}
+              margin="normal"
+              fullWidth
+              type="password"
+            />
+
+            <Button
+              color="primary"
+              variant="contained"
+              size="large"
+              type="submit"
+            >
+              Submit
+            </Button>
+
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </Form>
+        )}
+      </Formik>
     </Container>
   );
 };
